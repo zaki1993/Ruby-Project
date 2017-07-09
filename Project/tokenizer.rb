ddb1ddbc06c71638a32e485ef63e8bd8ba8eb8d8
zaki1993 <zaki1993@abv.bg>
2017-07-09 22:52:42 +0300

    Add define methods

--- Project/tokenizer.rb
+++ Project/tokenizer.rb
@@ -2,15 +2,16 @@
 class Tokenizer
   def initialize
     @tokens = []
-    @def_functions = []
-    File.readlines('defined_functions.txt').each { |f| @def_functions << f }
   end
 
   def tokenize(token)
-    @tokens = []
+    reset
     split_token token
-    puts @tokens.size
-    puts @tokens
+    calc_input_val @tokens
+  end
+
+  def reset
+    @tokens = []
   end
 
   def split_token(token)
@@ -24,5 +25,61 @@ class Tokenizer
       end
     end
     @tokens.delete('')
+  end
+
+  def calc_input_val(tokens)
+    if tokens.is_a? Array
+      tokens.each do |token|
+        next if ['(', ')'].include? token
+        result = !File.readlines('functions.txt').grep(/#{token}/).empty?
+        next unless result
+        send(token.to_s, tokens)
+      end
+    else
+      get_token_raw_value tokens
+    end
+  end
+
+  def get_token_raw_value(token)
+
+  end
+
+  def define(tokens)
+    open_br = 0
+    tokens.each_with_index do |token, idx|
+      open_br += 1 if token == '('
+      next unless token == 'define'
+      fetch_define tokens, idx + 1, tokens.length - open_br - 1
+    end
+  end
+
+  def fetch_define(tokens, start_idx, end_idx)
+    if tokens[start_idx + 1] == '('
+      define_function tokens, start_idx, end_idx
+    else
+      define_var tokens, start_idx, end_idx
+    end
+  end
+
+  def define_var(tokens, start_idx, end_idx)
+    value =
+      if start_idx + 1 == end_idx
+        calc_input_val tokens[start_idx + 1]
+      else
+        calc_input_val tokens[start_idx + 1, end_idx]
+      end
+    set_var tokens[start_idx], value
+  end
+
+  def define_function(tokens, start_idx, end_idx)
+
+  end
+
+  def set_var(var, value)
+    instance_variable_set("@#{var}", value)
+  end
+
+  def get_var(var)
+    instance_variable_get("@#{var}")
   end
 end
