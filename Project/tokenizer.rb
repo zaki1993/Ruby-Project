0eaf0fb8586af42ae2c5c926cd8ade79e3c1e65b
zaki1993 <zaki1993@abv.bg>
2017-07-14 10:51:48 +0300

    Separate numbers functions

--- Project/tokenizer.rb
+++ Project/tokenizer.rb
@@ -1,3 +1,6 @@
+load 'validator.rb'
+load 'numbers.rb'
+
 # redefine method in Object class
 class Object
   def number?
@@ -38,14 +41,6 @@ module SchemeChecker
     instance_variable_defined?("@#{var}")
   end
 
-  def valid_var_name(var)
-    !var.match(/^[[:alpha:]]+$/).nil?
-  end
-
-  def valid_var(var)
-    (check_for_number var) || (check_for_string var) || (check_for_bool var)
-  end
-
   def divide_number(a, b)
     return a / b if (a / b).to_i.to_f == a / b.to_f
     a / b.to_f
@@ -55,6 +50,9 @@ end
 # Tokenizer class
 class Tokenizer
   include SchemeChecker
+  include Validator
+  include SchemeNumbers
+  
   def initialize
     @tokens = []
     @functions = { 'string-length' => 'strlen', 'string-upcase' => 'strupcase',
@@ -148,47 +146,6 @@ class Tokenizer
     end
   end
 
-  def calculate_value_arithmetic(tokens, result, sign)
-    until tokens.empty?
-      x, tokens = find_next_value tokens, true
-      case sign
-      when '+' then result += x
-      when '-' then result -= x
-      when '*' then result *= x
-      when '/' then result = divide_number(result, x)
-      end
-    end
-    result
-  end
-
-  def +(other)
-    other = other[2..other.size - 2]
-    return 0 if other.size.zero?
-    result, other = find_next_value other, true
-    calculate_value_arithmetic other, result, '+'
-  end
-
-  def -(other)
-    other = other[2..other.size - 2]
-    result, other = find_next_value other, true
-    calculate_value_arithmetic other, result, '-'
-  end
-
-  def *(other)
-    other = other[2..other.size - 2]
-    return 1 if other.empty?
-    result, other = find_next_value other, true
-    calculate_value_arithmetic other, result, '*'
-  end
-
-  # TODO: Division by zero
-  def /(other)
-    other = other[2..other.size - 2]
-    result = 1 if other.size == 1
-    result, other = find_next_value other, true if other.size > 1
-    calculate_value_arithmetic other, result, '/'
-  end
-
   def get_k_arguments(tokens, return_tokens, k, to_number)
     result = []
     while (k -= 1) >= 0
@@ -196,113 +153,6 @@ class Tokenizer
       result << x
     end
     result << tokens if return_tokens
-    result
-  end
-
-  def primary_func_parser(oper, x, y)
-    case oper
-    when 'remainder' then (x.abs % y.abs) * (x / x.abs)
-    when 'modulo' then x.modulo(y)
-    when 'quotient' then (x / y).floor
-    end
-  end
-
-  def primary_func_tokenizer(tokens, oper)
-    tokens = tokens[2..tokens.size - 2]
-    x, y, tokens = get_k_arguments tokens, true, 2, true
-    raise 'Too many arguments' unless tokens.empty?
-    primary_func_parser(oper, x, y)
-  end
-
-  def quotient(tokens)
-    primary_func_tokenizer(tokens, 'quotient')
-  end
-
-  def remainder(tokens)
-    primary_func_tokenizer(tokens, 'remainder')
-  end
-
-  def modulo(tokens)
-    primary_func_tokenizer(tokens, 'modulo')
-  end
-
-  def get_num_denom(tokens)
-    num, tokens = find_next_value tokens, true
-    return [num, 1] if tokens.empty?
-    denom, tokens = find_next_value tokens, true
-    raise 'Too much arguments' unless tokens.empty?
-    [num, denom]
-  end
-
-  def find_idx_numerators(tokens)
-    tokens[0] == '(' ? (find_matching_bracket_idx tokens, 0) + 1 : 1
-  end
-
-  def num_denom_helper(tokens)
-    if tokens.size == 1
-      tokens = tokens[0].split('/')
-    else
-      _, temp = find_next_value tokens, true
-      raise 'Too much arguments' unless temp[0] == '/' || temp.empty?
-      i = find_idx_numerators tokens
-      tokens.delete_at(i)
-    end
-    tokens
-  end
-
-  def numerator(tokens)
-    tokens = tokens[2..tokens.size - 2]
-    tokens = num_denom_helper tokens
-    (get_num_denom tokens)[0].to_num
-  end
-
-  def denominator(tokens)
-    tokens = tokens[2..tokens.size - 2]
-    tokens = num_denom_helper tokens
-    (get_num_denom tokens)[1].to_num
-  end
-
-  def get_one_arg_function(tokens)
-    tokens = tokens[2..tokens.size - 2]
-    x, tokens = find_next_value tokens, true
-    raise 'Too much arguments' unless tokens.empty?
-    x
-  end
-
-  def abs(tokens)
-    x = get_one_arg_function tokens
-    x.abs
-  end
-
-  def add1(tokens)
-    x = get_one_arg_function tokens
-    x + 1
-  end
-
-  def sub1(tokens)
-    x = get_one_arg_function tokens
-    x - 1
-  end
-
-  def min(tokens)
-    tokens = tokens[2..tokens.size - 2]
-    x, y, tokens = get_k_arguments tokens, true, 2, true
-    result = x < y ? x : y
-    until tokens.empty?
-      next_val, tokens = find_next_value tokens, true
-      result = next_val if result > next_val
-    end
-    result
-  end
-
-  def max(tokens)
-    tokens = tokens[2..tokens.size - 2]
-    x, y, tokens = get_k_arguments tokens, true, 2, true
-    result = x > y ? x : y
-    until tokens.empty?
-      next_val, tokens = find_next_value tokens, true
-      result = next_val if result < next_val
-    end
     result
   end
 
