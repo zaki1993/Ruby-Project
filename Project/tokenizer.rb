c6b7a910cc763fc8d526e28fabafc44bd44381bf
zaki1993 <zaki1993@abv.bg>
2017-07-16 18:27:44 +0300

    Separate strings from main program

--- Project/tokenizer.rb
+++ Project/tokenizer.rb
@@ -1,5 +1,6 @@
 load 'validator.rb'
 load 'numbers.rb'
+load 'strings.rb'
 
 # redefine method in Object class
 class Object
@@ -73,6 +74,7 @@ class Tokenizer
   include SchemeChecker
   include Validator
   include SchemeNumbers
+  include SchemeStrings
 
   def initialize
     @tokens = []
@@ -219,133 +221,13 @@ class Tokenizer
     (get_var var) == '#t' ? '#f' : '#t'
   end
 
-  def string_getter(tokens, get_tokens)
-    str, tokens = find_next_value tokens, false
-    raise 'String needed' unless check_for_string str
-    [str, get_tokens ? tokens : _]
-  end
-
-  def substring_builder(str, from, to)
-    '"' + (str[1..-2])[from..(to.nil? ? -1 : to - 1)] + '"'
-  end
-
-  def substring(tokens)
-    tokens = tokens[2..-2]
-    str, tokens = string_getter tokens, true
-    from, tokens = find_next_value tokens, true
-    to, tokens = find_next_value tokens, true unless tokens.empty?
-    raise 'Too much arguments' unless tokens.empty?
-    substring_builder str, from, to
-  end
-
-  def string?(tokens)
-    tokens = tokens[2..-2]
-    str, tokens = find_next_value tokens, false
-    raise 'Too much arguments' unless tokens.empty?
-    result = check_for_string str
-    result ? '#t' : '#f'
-  end
-
-  def strlen(tokens)
-    tokens = tokens[2..-2]
-    str, tokens = string_getter tokens, true
-    raise 'Too much arguments' unless tokens.empty?
-    str[1..-2].length
-  end
-
-  def strupcase(tokens)
-    tokens = tokens[2..-2]
-    str, tokens = string_getter tokens, true
-    raise 'Too much arguments' unless tokens.empty?
-    str.upcase
-  end
-
-  def strdowncase(tokens)
-    tokens = tokens[2..-2]
-    str, tokens = string_getter tokens, true
-    raise 'Too much arguments' unless tokens.empty?
-    str.downcase
-  end
-
-  def strcontains(tokens)
-    tokens = tokens[2..-2]
-    string, tokens = string_getter tokens, true
-    to_check, tokens = string_getter tokens, true
-    raise 'Too much arguments' unless tokens.empty?
-    result = string.include? to_check[1..-2]
-    result ? '#t' : '#f'
-  end
-
-  def remove_carriage(str)
-    str = str[1..-2]
-    str.gsub('\n', '').gsub('\r', '').gsub('\t', '').strip.squeeze(' ')
-  end
-
-  def strsplit(tokens)
-    tokens = tokens[2..-2]
-    str, tokens = string_getter tokens, true
-    raise 'Too much arguments' unless tokens.empty?
-    str = remove_carriage str
-    '\'(' + str.split(' ').map { |s| '"' + s + '"' }.join(' ') + ')'
-  end
-
-  def build_symbol(char)
-    '#\\' + (char == ' ' ? 'space' : char)
-  end
-
-  def strlist(tokens)
-    tokens = tokens[2..-2]
-    str, tokens = string_getter tokens, true
-    raise 'Too much arguments' unless tokens.empty?
-    '\'(' + str[1..-2].chars.map { |c| build_symbol c }.join(' ') + ')'
-  end
-
-  def strreplace(tokens)
-    tokens = tokens[2..-2]
-    string, tokens = string_getter tokens, true
-    to_replace, tokens = string_getter tokens, true
-    replace_with, tokens = string_getter tokens, true
-    raise 'Too much arguments' unless tokens.empty?
-    string.gsub(to_replace[1..-2], replace_with[1..-2])
-  end
-
-  def strprefix(tokens)
-    tokens = tokens[2..-2]
-    string, tokens = string_getter tokens, true
-    to_check, tokens = string_getter tokens, true
-    raise 'Too much arguments' unless tokens.empty?
-    result = string[1..-1].start_with? to_check[1..-2]
-    result ? '#t' : '#f'
-  end
-
-  def strsufix(tokens)
-    tokens = tokens[2..-2]
-    string, tokens = string_getter tokens, true
-    to_check, tokens = string_getter tokens, true
-    raise 'Too much arguments' unless tokens.empty?
-    result = string[1..-1].end_with? to_check[1..-2]
-    result ? '#t' : '#f'
-  end
-
-  def build_next_value_as_string(tokens)
-    if tokens[0] == '('
-      idx = find_matching_bracket_idx tokens, 0
-      result = tokens[0..idx].join(' ').gsub('( ', '(').gsub(' )', ')')
-      [result, tokens[idx + 1..-1]]
-    else
-      [tokens[0], tokens[1..-1]]
-    end
-  end
-
   def evaluate_list(tokens)
     find_all_values tokens
   end
 
   def do_not_evaluate_list(tokens)
     result = []
-    puts tokens.to_s
     until tokens.empty?
-      puts tokens.to_s
       value, tokens = build_next_value_as_string tokens
       value = value[1..-2] if check_for_string value
       result << value
@@ -357,7 +239,6 @@ class Tokenizer
     if tokens[0..1].join == '(list'
       evaluate_list tokens[2..-2]
     else
-      puts tokens.to_s
       do_not_evaluate_list tokens[2..-2]
     end
   end
@@ -368,22 +249,6 @@ class Tokenizer
     elsif tokens[1] == '('
       find_matching_bracket_idx tokens, 1
     end
-  end
-
-  def find_delimeter(tokens)
-    return ' ' if tokens.empty?
-    result = (find_next_value tokens, false)[0]
-    valid = check_for_string result
-    valid ? result : (raise 'String needed')
-  end
-
-  def strjoin(tokens)
-    tokens = tokens[2..-2]
-    idx = find_idx_for_list tokens
-    raise 'List expected' unless tokens[0..idx].list?
-    values = find_to_evaluate_or_not tokens[0..idx]
-    delimeter = find_delimeter tokens[idx + 1..-1]
-    values.join delimeter[1..-2]
   end
 
   def find_all_values(tokens)
