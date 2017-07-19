cd882677043f93edbdc498fe159863e4ca572978
zaki1993 <zaki1993@abv.bg>
2017-07-19 16:25:56 +0300

    Optimize booleans

--- Project/boolean.rb
+++ Project/boolean.rb
@@ -1,30 +1,14 @@
-# Helper functions for SchemeBooleans
-module SchemeBooleansHelper
-  def fetch_not(tokens)
-    if tokens[0] == '('
-      res = calc_input_val tokens[0..tokens.size - 1]
-      not_var res
-    else
-      tokens.size == 1 ? (not_var tokens[0]) : (raise 'Incorrect parameter')
-    end
-  end
-
-  def not_var(var)
-    raise 'Incorrect boolean' unless check_for_bool var
-    (get_var var) == '#t' ? '#f' : '#t'
-  end
-end
-
 # Scheme booleans module
 module SchemeBooleans
   include SchemeBooleansHelper
   def equal?(other)
-    first, second, other = (get_k_arguments other, true, 2, false)
-    raise 'Too many arguments' unless other.empty?
-    first.to_s == second.to_s ? '#t' : '#f'
+    raise 'Incorect number of arguments' if other.size != 2
+    other[0].to_s == other[1].to_s ? '#t' : '#f'
   end
 
-  def not(tokens)
-    fetch_not tokens
+  def not(other)
+    raise 'Incorect number of arguments' if other.size != 1
+    raise 'Boolean needed' unless check_for_bool other[0]
+    other[0] == '#t' ? '#f' : '#t'
   end
 end
