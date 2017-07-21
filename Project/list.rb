78c8f834eb74eb995e0eb0dd59a3edf19aecd76f
zaki1993 <zaki1993@abv.bg>
2017-07-21 14:19:16 +0300

    Reverse changes about reverse

--- Project/list.rb
+++ Project/list.rb
@@ -169,10 +169,8 @@ module SchemeLists
   end
 
   def reverse(other)
-    raise 'Incorrect number of arguments' unless other.size == 1
-    value = split_list_string other[0]
-    raise 'Invalid argument' unless value.pair? || value.list?
-    build_list value[2..-2].reverse
+    value = find_list_function_value other
+    build_list value.reverse
   end
 
   def map(other)
