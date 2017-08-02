6298a5c5b1cc177ede7a0b482977038f9788b308
zaki1993 <zaki1993@abv.bg>
2017-08-02 16:50:57 +0300

    Update boolean.rb

--- Project/lisp-interpreter/lib/lisp/interpreter/boolean.rb
+++ Project/lisp-interpreter/lib/lisp/interpreter/boolean.rb
@@ -1,16 +1,5 @@
-# Scheme booleans module
-module SchemeBooleans
-  def equal?(other)
-    raise 'Incorrect number of arguments' if other.size != 2
-    other[0].to_s == other[1].to_s ? '#t' : '#f'
-  end
-
-  def not(other)
-    raise 'Incorrect number of arguments' if other.size != 1
-    raise 'Invalid data type' unless check_for_bool other[0]
-    other[0] == '#t' ? '#f' : '#t'
-  end
-
+# Scheme booleans helper
+module SchemeBooleansHelper
   def if_idx_helper(other)
     if other[0] == '('
       idx = find_bracket_idx other, 0
@@ -28,7 +17,22 @@ module SchemeBooleans
       (find_next_value other)[0]
     end
   end
+end
 
+# Scheme booleans module
+module SchemeBooleans
+  include SchemeBooleansHelper
+  def equal?(other)
+    raise 'Incorrect number of arguments' if other.size != 2
+    other[0].to_s == other[1].to_s ? '#t' : '#f'
+  end
+
+  def not(other)
+    raise 'Incorrect number of arguments' if other.size != 1
+    raise 'Invalid data type' unless check_for_bool other[0]
+    other[0] == '#t' ? '#f' : '#t'
+  end
+  
   def if(other)
     raise 'Incorrect number of arguments' if other.empty?
     expr, other = find_next_value other
