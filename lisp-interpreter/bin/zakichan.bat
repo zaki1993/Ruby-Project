f57d2847f3ca3534c99baa69ac291a53cecd430e
Zaki Petrov <zaki1993@abv.bg>
2017-09-01 22:30:33 +0300

    Fix bat files

--- lisp-interpreter/bin/zakichan.bat
+++ lisp-interpreter/bin/zakichan.bat
@@ -1,2 +1,12 @@
 @echo off
+call rake spec > log 2> NUL
+if %ERRORLEVEL% == 0 goto :run
+echo Could not start the application.
+echo Please check the log file.
+goto :end
+
+:run
 start cmd /C "title lisp-interpreter & ruby ../lib/lisp/interpreter/run.rb' 2> log & echo Error occured. & timeout 2 > NUL & echo Logging into file 'log'. & timeout 2 > NUL & pause"
+goto: end
+
+:end
